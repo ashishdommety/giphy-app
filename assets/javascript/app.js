ed89be4b4c7a2b7a4f23d79ea9641a5117897ed6
ashishdommety <ashishuiux@gmail.com>
2017-05-30 18:41:50 -0700

    Fully functional.. styling and extra features left

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,60 @@
-console.log('js file linked');
+$(document).ready(function(){
+  var topics = ['cat','dog','dolphin','horse','skunk'];
+
+  var flip = true;
+
+  function displayTopic(){
+    $('.gifs').empty();
+      var topic = $(this).attr('data-name');
+      var gifURL = 'https://api.giphy.com/v1/gifs/search?q='+ topic +'&limit=10&api_key=dc6zaTOxFJmzC';
+      $.ajax({
+        method:'GET',
+        url: gifURL
+      }).done(function(response){
+        for( var i = 0; i < 10; i++){
+          var still = response.data[i].images.fixed_height_still.url;
+          var pic = $('<img>');
+          pic.attr('src', still);
+          pic.addClass('animals');
+          pic.attr('data-num',i);
+          $('.gifs').append(pic);
+        }
+        $(document).on('click','.animals',function(){
+          if(flip){
+            var num = $(this).data('num');
+            var link = response.data[num].images.fixed_height.url;
+            $(this).attr('src',link);
+            flip = false;
+          }
+          else{
+            var num = $(this).data('num');
+            var still = response.data[num].images.fixed_height_still.url;
+            $(this).attr('src',still);
+            flip = true;
+          }
+        });
+      });
+  }
+
+  function renderButtons(){
+    for( var i = 0; i < topics.length; i++){
+      var a = $('<button>');
+      a.addClass('animal');
+      a.attr('data-name',topics[i]);
+      a.text(topics[i]);
+      $('.top').append(a);
+    }
+  }
+  renderButtons();
+
+  $('#add-animal').on('click', function(event){
+    event.preventDefault();
+    $('.top').empty();
+    var animal = $('#animal-input').val().trim();
+    topics.push(animal);
+    renderButtons();
+    $('#animal-input').val('')
+  })
+
+  $(document).on('click','.animal',displayTopic);
+});
