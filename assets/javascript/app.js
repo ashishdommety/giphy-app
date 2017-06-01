cb27af71e6a9ab8435bcbcaaf5f9c1321ac95c3b
ashishdommety <ashishuiux@gmail.com>
2017-05-31 17:29:35 -0700

    Fully functional, minor bugs still left

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,88 @@
-$(document).ready(function(){
-  var topics = ['cat','dog','dolphin','horse','skunk'];
+$(document).ready(function() {
+  var topics = ['cat', 'dog', 'dolphin', 'horse', 'skunk'];
+  var num;
+  var flip;
+  var changeBack;
 
-  function displayTopic(){
+  function displayTopic() {
     $('.gifs').empty();
-      var topic = $(this).attr('data-name');
-      var gifURL = 'https://api.giphy.com/v1/gifs/search?q='+ topic +'&limit=10&api_key=dc6zaTOxFJmzC';
-      $.ajax({
-        method:'GET',
-        url: gifURL
-      }).done(function(response){
-        for( var i = 0; i < 10; i++){
-          var still = response.data[i].images.fixed_height_still.url;
-          var pic = $('<img>');
-          pic.attr('src', still);
-          pic.addClass('animals');
-          pic.attr('data-num',i);
-          $('.gifs').append(pic);
+    var topic = $(this).attr('data-name');
+    var gifURL = 'https://api.giphy.com/v1/gifs/search?q=' + topic + '&limit=12&api_key=dc6zaTOxFJmzC';
+    $.ajax({
+      method: 'GET',
+      url: gifURL
+    }).done(function(response) {
+      for (var i = 0; i < 12; i++) {
+        var still = response.data[i].images.fixed_height_still.url;
+        var pic = $('<img>');
+        pic.attr('src', still);
+        pic.addClass('animals');
+        pic.attr('data-num', i);
+        $('.gifs').append(pic);
+      }
+      flip = true;
+      $('.animals').off().on('click', function() {
+        if (flip) {
+          num = $(this).data('num');
+          var link = response.data[num].images.fixed_height.url;
+          $(this).attr('src', link);
+          flip = false;
+        } else {
+          num = $(this).data('num');
+          var still = response.data[num].images.fixed_height_still.url;
+          $(this).attr('src', still);
+          flip = true;
         }
-        var flip = true;
-        $(document).on('click','.animals',function(){
-          if(flip){
-            var num = $(this).data('num');
-            var link = response.data[num].images.fixed_height.url;
-            $(this).attr('src',link);
-            flip = false;
-          }
-          else{
-            var num = $(this).data('num');
-            var still = response.data[num].images.fixed_height_still.url;
-            $(this).attr('src',still);
-            flip = true;
-          }
-        });
       });
+      $('.animals').draggable({
+        start: function getLink(event,ui){
+         num = $(this).data('num');
+        },
+        containment:'window',
+        cursor:'move',
+        snap:'.drops',
+        stack:'img',
+        distance:0,
+        revert:true
+      });
+      $('.drops').droppable({
+        drop: function handleDrop(event,ui){
+          var draggable = ui.draggable;
+          var still = response.data[num].images.fixed_height_still.url;
+          var link = response.data[num].images.fixed_height.url
+          $(this).attr('src', still);
+          $(this).parent().attr('href',link);
+        }
+      });
+      $('.drops').draggable({
+        start: function getElem(){
+          changeBack = $(this);
+        },
+        cursor:'move',
+        revert: true,
+      });
+      $('.gifs').droppable({
+        drop: function returnGif(event,ui){
+          changeBack.attr('src', 'assets/images/placeholder.jpg');
+        }
+      });
+    });
   }
 
-  function renderButtons(){
-    for( var i = 0; i < topics.length; i++){
+  function renderButtons() {
+    for (var i = 0; i < topics.length; i++) {
       var a = $('<button>');
       a.addClass('animal');
-      a.attr('data-name',topics[i]);
+      a.attr('data-name', topics[i]);
       a.text(topics[i]);
       $('.top').append(a);
     }
   }
   renderButtons();
 
-  $('#add-animal').on('click', function(event){
+  $('#add-animal').on('click', function(event) {
     event.preventDefault();
+    $('.gifs').css('height',null);
     $('.top').empty();
     var animal = $('#animal-input').val().trim();
     topics.push(animal);
@@ -55,5 +90,5 @@ $(document).ready(function(){
     $('#animal-input').val('')
   })
 
-  $(document).on('click','.animal',displayTopic);
+  $(document).on('click', '.animal', displayTopic);
 });
