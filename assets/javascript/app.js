f591dd73858f5381598487ad8276de6ae1536dee
ashishdommety <ashishuiux@gmail.com>
2017-06-02 14:12:37 -0700

    Final touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   var topics = ['cat', 'dog', 'dolphin', 'horse', 'skunk'];
   var num;
-  var flip;
 
   function displayTopic() {
     $('.gifs').empty();
+    $('.gifs').css('height', null);
     var topic = $(this).attr('data-name');
     var gifURL = 'https://api.giphy.com/v1/gifs/search?q=' + topic + '&limit=12&api_key=dc6zaTOxFJmzC';
     $.ajax({
@@ -17,23 +17,27 @@ $(document).ready(function() {
         pic.attr('src', still);
         pic.addClass('animals');
         pic.attr('data-num', i);
+        pic.attr('data-state', 'still');
         $('.gifs').append(pic);
       }
 
       //adding the click toggle and the dragging options
-      flip = true;
       $('.animals').on('click', function() {
-          if (flip) {
-            num = $(this).data('num');
-            var link = response.data[num].images.fixed_height.url;
-            $(this).attr('src', link);
-            flip = false;
-          } else {
-            num = $(this).data('num');
-            var still = response.data[num].images.fixed_height_still.url;
-            $(this).attr('src', still);
-            flip = true;
-          }
+        var state = $(this).attr('data-state');
+        console.log(state);
+        if (state === 'still') {
+          num = $(this).data('num');
+          var link = response.data[num].images.fixed_height.url;
+          $(this)
+            .attr('src', link)
+            .attr('data-state', 'animate');
+        } else {
+          num = $(this).data('num');
+          var still = response.data[num].images.fixed_height_still.url;
+          $(this)
+            .attr('src', still)
+            .attr('data-state', 'still');
+        }
       }).draggable({
         containment: 'window',
         cursor: 'move',
@@ -56,24 +60,51 @@ $(document).ready(function() {
 
   function renderButtons() {
     for (var i = 0; i < topics.length; i++) {
-      var a = $('<button>');
-      a.addClass('animal');
-      a.attr('data-name', topics[i]);
-      a.text(topics[i]);
-      $('.top').append(a);
+      var $a = $('<button>');
+      $a.addClass('animal');
+      $a.attr('data-name', topics[i]);
+      $a.text(topics[i]);
+      $('.top').append($a);
     }
   }
   renderButtons();
 
-  $('#add-animal').on('click', function(event) {
+  $('.add-animal').on('click', function(event) {
     event.preventDefault();
     $('.gifs').css('height', null);
     $('.top').empty();
     var animal = $('#animal-input').val().trim();
     topics.push(animal);
     renderButtons();
-    $('#animal-input').val('')
+    $('#animal-input').val('');
   });
 
+  //click for buttons
   $(document).on('click', '.animal', displayTopic);
+
+  //effects for animal buttons
+  $(document).on('mousedown', '.animal, .add-animal', function() {
+    $(this).css({
+      'top': '5px',
+      'left': '5px',
+      'box-shadow': 'none'
+    });
+  });
+  $(document).on('mouseup', '.animal, .add-animal', function() {
+    var hexValue = getHexValue($(this));
+    $(this).css({
+      'top': '0px',
+      'left': '0px',
+      'box-shadow': '5px 5px 0px ' + hexValue
+    });
+  });
+
+  //function to get the hex value
+  function getHexValue($element) {
+    if ($element.hasClass('animal')) {
+      return '#4f2d42';
+    } else if ($element.hasClass('add-animal')) {
+      return '#668f9e';
+    }
+  }
 });
